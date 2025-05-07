d23bcef533a81fb331334798ee3ed5a952b28b29
leviking12 <leviking.lk@gmail.com>
2025-05-06 23:17:05 -0400

    fixing bugs

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -2,8 +2,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import { Sequelize } from 'sequelize'
-import { UserFactory } from './user.js'
-import { TicketFactory } from './ticket.js'
+import { UserFactory } from './user'
+import { TicketFactory } from './ticket'
 
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import path from 'path'
-import express from 'express'
-import routes from './routes/index.js'
-import { sequelize } from './models/index.js'
+import express, { Request, Response } from 'express'
+import { fileURLToPath } from 'url'
+import routes from './routes/index'
+import { sequelize } from './models/index'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 const PORT = Number(process.env.PORT || 3001)
@@ -13,7 +17,7 @@ const clientDist = path.resolve(__dirname, '../../client/dist')
 app.use(express.static(clientDist))
 app.use(express.json())
 app.use(routes)
-app.get('*', (req, res) => {
+app.get('*', (req: Request, res: Response) => {
   res.sendFile(path.join(clientDist, 'index.html'))
 })
 
@@ -21,8 +25,11 @@ sequelize
   .authenticate()
   .then(() => sequelize.sync())
   .then(() => {
-    app.listen(PORT)
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`)
+    })
   })
-  .catch(() => {
+  .catch((err: any) => {
+    console.error('Unable to start server:', err)
     process.exit(1)
   })
